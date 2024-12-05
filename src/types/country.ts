4e77395560e500334af6f17478ae37f1a1e39db9
Anton Lazurko <antla@ciklum.com>
2024-12-05 14:23:53 +0200

    feat: add base types

---
+++ src/types/country.ts
@@ -0,0 +1,27 @@
+import { Region } from "./regions";
+
+
+type Currencies = { name: string, symbol: string, code: string };
+type Language = {
+    iso639_1: string,
+    iso639_2: string,
+    name: string,
+    nativeName: string
+  }
+
+export type Country = {
+    name: string;
+    nativeName: string;
+    flag: [
+        {png: string},
+        {svg: string}
+    ];
+    region: Region;
+    subregion: string;
+    capital: string;
+    population: number;
+    topLevelDomain: string[];
+    currencies: Currencies[];
+    languages: Language[];
+    borders: string[];
+}
